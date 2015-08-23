89d4d783550dacb014699ee4434402312343e371
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-08-23 15:39:10 +0200

    used ng-include directive instead of a custom directive for new card form

--- public/js/directives/new_card_form.js
+++
@@ -1,8 +0,0 @@
-angular.module('trelloRedmine')
-.directive('newCardForm', function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'views/templates/new_card_form.html'
-    };
-});
