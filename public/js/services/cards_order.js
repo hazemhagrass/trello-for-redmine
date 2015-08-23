5901b8ed3995d1495490f6567d3e185d99ffa197
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-08-23 14:51:50 +0200

    BUG FIX: inserting 3 in that array [1, 2, 4] would have inserted it in the last element, now it is inserted in the correct place

--- public/js/services/cards_order.js
+++ public/js/services/cards_order.js
@@ -1,14 +1,18 @@
 angular.module('trelloRedmine')
 .service('cardsOrder', function (){
 
-    this.insertInOrder = function(array, element){
+    return {
+        insertInOrder: insertInOrder,
+        reorderWidgetElement: reorderWidgetElement
+    }
+    
+    function insertInOrder(array, element) {
         var i = 0;
-        while(i < array.length && element.id < array[i++].id);
-        // i is incremented by 1 when reaching the required place so we insert in i-1, CORNER CASE the element is with the least id, so we don't decrement i
-        i === array.length ? array.splice(i, 0, element) : array.splice(i-1, 0, element);
+        while(i < array.length && element.id < array[i].id) { i++; }
+        array.splice(i, 0, element);
     }
 
-    this.reorderWidgetElement = function(array, id){
+    function reorderWidgetElement(array, id) {
         var card = null;
         array.some(function(element, index){
             if(element.id == id){
@@ -17,7 +21,7 @@ angular.module('trelloRedmine')
                 return true;
             }
         });
-        this.insertInOrder(array, card);
+        insertInOrder(array, card);
     }
 
 });
