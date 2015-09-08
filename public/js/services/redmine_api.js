ced7d38b48fcbc05eba232cd089fa8c8bf48b810
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-09-08 20:51:50 +0200

    Removing hardcoded project members + commenting task status change due to a bug in task modal when card is moved to another widget

--- public/js/services/redmine_api.js
+++ public/js/services/redmine_api.js
@@ -53,35 +53,35 @@ angular.module('trelloRedmine')
       user_projects.some(function(project) {
         if(project.id == project_id) {
           angular.extend(selected_project, project);
-          if(project_id == 209){
-            // testing on an unauthorized user
-            selected_project.members = [
-              {
-                "id": 34,
-                "name": "Hazem Hagrass"
-              },
-              {
-                "id": 99,
-                "name": "Kareem Fikry"
-              },
-              {
-                "id": 143,
-                "name": "Ahmad Gaber"
-              },
-              {
-                "id": 115,
-                "name": "Ahmad Ali"
-              },
-              {
-                "id": 42,
-                "name": "Ahmed Moawad"
-              },
-              {
-                "id": 139,
-                "name": "Ossama Sanosi"
-              }
-            ]
-          }
+          // if(project_id == 209){
+          //   // testing on an unauthorized user
+          //   selected_project.members = [
+          //     {
+          //       "id": 34,
+          //       "name": "Hazem Hagrass"
+          //     },
+          //     {
+          //       "id": 99,
+          //       "name": "Kareem Fikry"
+          //     },
+          //     {
+          //       "id": 143,
+          //       "name": "Ahmad Gaber"
+          //     },
+          //     {
+          //       "id": 115,
+          //       "name": "Ahmad Ali"
+          //     },
+          //     {
+          //       "id": 42,
+          //       "name": "Ahmed Moawad"
+          //     },
+          //     {
+          //       "id": 139,
+          //       "name": "Ossama Sanosi"
+          //     }
+          //   ]
+          // }
           return true;
         }
       });
