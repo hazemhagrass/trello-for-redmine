a99900cf95f437586ce0cbc87747475575fe53e1
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-09-09 18:07:26 +0200

    Fixing new_task_ctrl.js indentations

--- public/js/controllers/new_task_ctrl.js
+++ public/js/controllers/new_task_ctrl.js
@@ -1,56 +1,56 @@
 angular.module('trelloRedmine')
 .controller('NewTaskCtrl', ['$scope', '$modal', '$localStorage', 'redmineService', 'cardsHelpers', 'toaster',
-    function($scope, $modal, $localStorage, redmineService, cardsHelpers, toaster) {
+  function($scope, $modal, $localStorage, redmineService, cardsHelpers, toaster) {
 
-        $scope.newTask = initiateTask();
+    $scope.newTask = initiateTask();
 
-        $scope.addNewSubTask = function(card) {
-            $modalInstance = $modal.open({
-                scope: $scope,
-                templateUrl: 'views/templates/new_task.html',
-                backdropClass: "backdrop-fix"
-            });
-        }
-
-        $scope.createTask = function(card, newTask) {
-            toaster.pop({ type: 'info',
-              title: 'A new task is being added to card ' + card.id + '.',
-              showCloseButton: true
-            });
-            var assigned_to_id = newTask.assigned_to_id ? newTask.assigned_to_id : $localStorage.user_id;
-            var priority_to_id = (card.priority) ? card.priority.id : '';
-            
-            newTask.priority_id = priority_to_id;
-            newTask.assigned_to_id = assigned_to_id;
-            newTask.parent_issue_id = card.id;
-
-            $scope.dismiss();
-            
-            redmineService.createTask(newTask)
-            .then(function (result) {
-                var issue = result.data.issue;
-                issue.assigned_to.mail = issue.assigned_to.name.replace(/ /g, '.').toLowerCase() + '@badrit.com';
-                card.subTasks.unshift(issue);
-                cardsHelpers.calculateProgress(card);
-                toaster.pop({ type: 'info',
-                  title: 'A new task has been added successfully.',
-                  showCloseButton: true
-                });
-            }).finally( function() {
-                $scope.newTask = initiateTask();
-            });
-        };
-
-        function initiateTask(){
-            return {
-                subject: "",
-                description: "",
-                priority_id: 0,
-                project_id: $scope.project_id,
-                parent_issue_id: 0,
-                tracker_id: 4,
-                assigned_to_id: $localStorage.user_id
-            };
-        }
+    $scope.addNewSubTask = function(card) {
+      $modalInstance = $modal.open({
+        scope: $scope,
+        templateUrl: 'views/templates/new_task.html',
+        backdropClass: "backdrop-fix"
+      });
     }
+
+    $scope.createTask = function(card, newTask) {
+      toaster.pop({ type: 'info',
+        title: 'A new task is being added to card ' + card.id + '.',
+        showCloseButton: true
+      });
+      var assigned_to_id = newTask.assigned_to_id ? newTask.assigned_to_id : $localStorage.user_id;
+      var priority_to_id = (card.priority) ? card.priority.id : '';
+
+      newTask.priority_id = priority_to_id;
+      newTask.assigned_to_id = assigned_to_id;
+      newTask.parent_issue_id = card.id;
+
+      $scope.dismiss();
+
+      redmineService.createTask(newTask)
+      .then(function (result) {
+        var issue = result.data.issue;
+        issue.assigned_to.mail = issue.assigned_to.name.replace(/ /g, '.').toLowerCase() + '@badrit.com';
+        card.subTasks.unshift(issue);
+        cardsHelpers.calculateProgress(card);
+        toaster.pop({ type: 'info',
+          title: 'A new task has been added successfully.',
+          showCloseButton: true
+          });
+      }).finally( function() {
+        $scope.newTask = initiateTask();
+      });
+    };
+
+    function initiateTask(){
+      return {
+        subject: "",
+        description: "",
+        priority_id: 0,
+        project_id: $scope.project_id,
+        parent_issue_id: 0,
+        tracker_id: 4,
+        assigned_to_id: $localStorage.user_id
+      };
+    }
+  }
 ]);
