b14f134d855d4e5d37cb94d99bfc13e19e95bead
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-09-14 14:26:14 +0200

    Adding progress bar to edit_card instead of a percentage

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-	angular.module('trelloRedmine', ['gridster', 'ui.bootstrap.accordion', 'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ngRoute', 'ui.sortable', 'ngAnimate', 'mgcrea.ngStrap.popover', 'mgcrea.ngStrap.tooltip',
+	angular.module('trelloRedmine', ['gridster', 'ui.bootstrap.accordion', 'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ui.bootstrap.progressbar', 'ngRoute', 'ui.sortable', 'ngAnimate', 'mgcrea.ngStrap.popover', 'mgcrea.ngStrap.tooltip',
                                     'ui.gravatar', 'xeditable', 'ngSanitize', 'ngStorage', 'angularFileUpload', 'toaster'])
 		.config(['$routeProvider', '$locationProvider',
 			function($routeProvider, $locationProvider) {
