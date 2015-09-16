5702e25d79d65951d152f9c77b032f6d2ac66d91
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-09-16 11:53:44 +0200

    Adding animation to the newly shifted activities at the front of the list

--- public/js/services/activities_helpers.js
+++ public/js/services/activities_helpers.js
@@ -12,11 +12,24 @@ angular.module('trelloRedmine')
     redmineService.getActivities(redmineAPI.selected_project.id)
     .then(function (result) {
       var data = JSON.parse(result.data);
+      var first_activity_time = redmineAPI.activities[0].time;
+      var first_activity_title = redmineAPI.activities[0].title;
+      var matched_flag = false;
       // NOTE writing activities = [] would make a new reference and hence the scope won't have the same reference as this one
       redmineAPI.activities.length = 0;
-      angular.forEach(data.activities, function(activity){
+      data.activities.forEach( function(activity) {
+
+        if( activity.time == first_activity_time && activity.title == first_activity_title) {
+          matched_flag = true;
+        }
+        if( !matched_flag ) {
+          activity.animate = true;
+        }
+
         redmineAPI.activities.push(activity);
-      })
+
+      });
+
     });
   };
 
